d829d0cfb2234c07f3793eaad4bc665ab6a71f9e
Kieran Harvie <cleisthenes.h@gmail.com>
2024-05-16 17:46:10 +1000

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+<!-- 
+Copyright 2024 Kieran Harvie.
+All rights reserved.
+Use of this source code is governed by an MIT-style license that can be found in the LICENSE file.
+-->
+
+This repo is a quick treemap visualization of the expected value from a tour completion in TF2.
+It is based on and uses data from the most recent, at time of writing, [TheLetterZed video](https://www.youtube.com/watch?v=kPR0KjP4hC8).
+
+Basically a python script to transpose the data in the [spreadsheet](https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqa3JhNkVYVGVaNW54c1ZDWW14SmdvUF9zREJpZ3xBQ3Jtc0ttWW0ySW5ETGJ4N3BOQm5SZ3VKclRxcUNucV9NNGNuTW5SU1RwcnVZZWZUMVk4WDJPX3R0Z1BWNnhIS0xjQTRnT0NvekpkeFV3QkJOMER4NmNBQXhZRHk0UklPUDl1dFAtOEVFR1Z3QnZ6cW1meHpqcw&q=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F1UvPC-FKVYYY8q8Zb18fjxb-AmZgZbQ471tu5TsjG3w0%2Fedit%3Fusp%3Dsharing&v=kPR0KjP4hC8) from the video description into the [squarify](https://github.com/laserson/squarify/tree/master) module. 
+
+![Australium](Australium.png)
+![GearGrinder](GearGrinder.png)
+![MechaEngine](MechaEngine.png)
+![OilSpill](OilSpill.png)
+![SteelTrap](SteelTrap.png)
+![TwoCities](TwoCities.png)
---
+++ main.py
@@ -0,0 +1,43 @@
+# Copyright 2024 Kieran Harvie.
+# All rights reserved.
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file.
+
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import squarify 
+import pandas as pd
+import seaborn as sb
+from textwrap import wrap
+
+excel = pd.read_excel('Expected Value of Mann vs. Machine.xlsx',sheet_name = None)
+
+def make_treeplot(sheet):
+    df = excel[sheet]
+    df.sort_values(by=["Expected Value (Keys)"],ascending=False)
+
+    rects = squarify.squarify(
+           squarify.normalize_sizes(df["Expected Value (Keys)"],1,1)
+           ,0,0,1,1)
+
+    transpose = ({
+            'p':(a['x'],a['y']),
+            'dx':a['dx'],
+            'dy':a['dy'],
+            'text':'\n'.join(wrap(b,15)),
+            'color':c
+            }for a,b,c in zip(rects,df["Item"],sb.color_palette("tab20",len(rects))))
+
+    plt.figure(figsize=(15,15))
+    ax = plt.gca()
+
+    for rect in transpose:
+        ax.add_patch(Rectangle(rect['p'],rect['dx'],rect['dy'],linewidth=1,edgecolor='black',facecolor=rect['color']))#,label=rect["text"]))
+        ax.annotate(rect["text"],rect['p'])
+
+    plt.title(sheet)
+    plt.axis('off')
+    plt.savefig(sheet + '.png')
+
+for sheet in {"OilSpill","SteelTrap","MechaEngine","TwoCities","GearGrinder","Australium"}:
+    make_treeplot(sheet)
